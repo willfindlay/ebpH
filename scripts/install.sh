af0880cda7babf2db4d051711f8e4bdae70ecf4b
HousedHorse <william@wfindlay.com>
2019-07-12 13:24:38 -0400

    install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env -S bash -e
 
 DIR=$(dirname $(readlink -f $0))/..
-cd $DIR
+INSTALLDIR=/opt/ebpH
 
-pushd .
+cd $DIR
+sudo chown root:root ebpH_command && sudo chmod 700 ebpH_command
+
+sudo mkdir -p $INSTALLDIR
+sudo cp -r ./* $INSTALLDIR
+cd $INSTALLDIR
+sudo ln -sf $(readlink -f ./ebpH) /bin/ebpH
