ac6375425b07eb43c35309b7b145a2a8dcd82efd
willfindlay <william@williamfindlay.com>
2020-05-06 13:03:29 -0400

    Sync structs.py with bpf_program.h

--- ebpH/structs.py
+++ ebpH/structs.py
@@ -24,7 +24,7 @@ from ebpH import defs
 
 EBPH_NUM_SYSCALLS = defs.bpf_params['EBPH_NUM_SYSCALLS']
 EBPH_SEQLEN = 9
-EBPH_SEQSTACK_SIZE = 6
+EBPH_SEQSTACK_SIZE = 2
 EBPH_LOCALITY_WIN = 128
 EBPH_FILENAME_LEN = 128
 TASK_COMM_LEN = 16
@@ -84,18 +84,7 @@ class EBPHSequence(ct.Structure):
     This class MUST be kept in sync with src/bpf/bpf_program.h
     """
     _fields_ = [
-            ('seq', ct.c_long * EBPH_SEQLEN),
-            ('count', ct.c_uint8),
-            ]
-
-class EBPHSequenceStack(ct.Structure):
-    """
-    ctypes structure representing struct ebpH_sequence_stack from src/bpf/bpf_program.h
-
-    This class MUST be kept in sync with src/bpf/bpf_program.h
-    """
-    _fields_ = [
-            ('seq', EBPHSequence * EBPH_SEQSTACK_SIZE),
+            ('seq', ct.c_long * EBPH_SEQLEN * EBPH_SEQSTACK_SIZE),
             ('top', ct.c_uint8),
             ('should_pop', ct.c_uint8),
             ]
@@ -108,7 +97,7 @@ class EBPHProcess(ct.Structure):
     """
     _fields_ = [
             ('alf', EBPHLocality),
-            ('stack', EBPHSequenceStack),
+            ('seq', EBPHSequence),
             ('pid', ct.c_uint32),
             ('tid', ct.c_uint32),
             ('profile_key', ct.c_uint64),
