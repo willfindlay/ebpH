ba4e296296b4ff9c1b7aaad47a9c57ce68a2e20c
William Findlay <william@williamfindlay.com>
2020-07-13 22:37:19 -0400

    Fix formatting

--- ebph/commands/ebph_ps.py
+++ ebph/commands/ebph_ps.py
@@ -9,12 +9,12 @@ from ebph import defs
 header = False
 
 def format_comm(comm):
-    return comm if len(comm) < 16 else ''.join(['...', comm[:-13]])
+    return comm if len(comm) < 24 else ''.join(['...', comm[-21:]])
 
 def print_profile_information(profile):
     comm = format_comm(profile["exe"])
     status = profile['status']
-    status = status.split('EBPH_PROFILE_STATUS.')[1]
+    status = status.split('EBPH_PROFILE_STATUS.')[1].capitalize()
     anomalies = profile['anomaly_count']
     train_count = profile['train_count']
     last_mod_count = profile['last_mod_count']
@@ -22,10 +22,10 @@ def print_profile_information(profile):
 
     global header
     if not header:
-        print(f"{'COMM':<16} {'STATUS':<20} {'TRAIN_COUNT':>12} {'LAST_MOD':>12} {'ANOMALIES':>12} {'NORMAL TIME':<16}")
+        print(f"{'COMM':<24} {'STATUS':<20} {'TRAIN_COUNT':>12} {'LAST_MOD':>12} {'ANOMALIES':>12} {'NORMAL TIME':<16}")
         header = True
 
-    print(f"{comm:<16} {status:<20} {train_count:>12} {last_mod_count:>12} "
+    print(f"{comm:<24} {status:<20} {train_count:>12} {last_mod_count:>12} "
             f"{anomalies:>12} {normal_time:<16}")
 
 
@@ -34,14 +34,9 @@ def main(args: Namespace):
         res = requests.get(f'http://localhost:{defs.EBPH_PORT}/profiles')
         if res.status_code != 200:
             print('Unable to get profiles', file=sys.stderr)
-        for p in json.loads(res.content):
+        for p in sorted(json.loads(res.content), key=lambda p: p['exe']):
             print_profile_information(p)
+    elif args.threads:
+        res = requests.get(f'http://localhost:{defs.EBPH_PORT}/processes')
     else:
         res = requests.get(f'http://localhost:{defs.EBPH_PORT}/processes')
-
-    if args.profiles:
-        print('profiles')
-    elif args.threads:
-        print('threads')
-    else:
-        print('processes')
