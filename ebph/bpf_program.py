fcca3352048f4a6c3d80f8c2270055596e08a9d3
William Findlay <william@williamfindlay.com>
2020-07-16 12:57:41 -0400

    Added license information to all important files

--- ebph/bpf_program.py
+++ ebph/bpf_program.py
@@ -1,3 +1,26 @@
+"""
+    ebpH (Extended BPF Process Homeostasis)  A host-based IDS written in eBPF.
+    Copyright (C) 2019-2020  William Findlay
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    A wrapper around the BPF program. Exposes methods for interacting
+    with it from userspace and for handling events.
+
+    2020-Jul-13  William Findlay  Created this.
+"""
+
 import os
 import time
 import atexit
