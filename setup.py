6412ae57e5291591d8791a58ff7868c4bbfb1908
William Findlay <william@williamfindlay.com>
2020-07-18 13:03:58 -0400

    Prepare for 0.3.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import re
 from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext
 
-version = '0.2.2'
+version = '0.3.0'
 
 
 class ct_build_ext(build_ext):
